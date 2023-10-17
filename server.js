2286244c1408a22bdddf2e99137756cd071becee
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-17 19:20:41 -0300

    Initial push

---
+++ server.js
@@ -0,0 +1,19 @@
+// Psudo-Code.
+
+// Options needed: Departments, Roles, Employees.
+// Function to create a new employee with all of the above added. This can be done with inquierer.
+// Each of these options will be a multi-selection. Department will give the option of choosing all departments and roles.
+
+
+// On application start. You will get the option of viewing departments, roles and employees.
+// Departments will show department name and ids.
+
+// on viewing roles, you will see the job title, id, department and salery.
+
+// On viewing employees, you will see first and last name, job title, department, salery and their manager.
+
+// Managers will be attached to departments. Note that you should be able to add new departments.
+
+// For extra credit, do I want to make a function that allows you to select the manager of each department. Upon change,
+// Should there be a function to warn that you are moving the admin title to someone else? Would be cool.
+
