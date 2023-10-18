7e577db7672848849f889955d370f6fd4306d4a3
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-17 23:01:23 -0300

    Building server.js framework. Added basics to roles.sql. Added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// This index will take the users input and parse it to move the user to the correct selection.
+
+const express = require('express')
+
+const userInput = 
+
+
+
+module.export()
--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // Function to create a new employee with all of the above added. This can be done with inquierer.
 // Each of these options will be a multi-selection. Department will give the option of choosing all departments and roles.
 
-
 // On application start. You will get the option of viewing departments, roles and employees.
 // Departments will show department name and ids.
 
@@ -17,3 +16,55 @@
 // For extra credit, do I want to make a function that allows you to select the manager of each department. Upon change,
 // Should there be a function to warn that you are moving the admin title to someone else? Would be cool.
 
+// Steps: Initialize> Initial menu prompt (Move to new file to keep things simple? Make new module)>
+// Set up api's and middleware to handle requests.
+const inquirer = require('inquirer');
+const express = require('express');
+app = express();
+
+
+
+
+
+
+const mainSelection = function mainPrompts(){
+    inquirer
+        .prompt([ 
+            {
+                type: 'list',
+                message: 'Welcome.',
+                name: 'selector',
+                choices: 
+                [
+                    {
+                    name: 'View all departments',
+                    value: 'viewDepartments' 
+                },
+                {
+                    name: 'View all employees',
+                    value: 'viewEmployees' 
+                },
+                {
+                    name: 'View all roles',
+                    value: 'viewRoles' 
+                },
+                {
+                    name: 'Add a Department',
+                    value: 'addDepartment' 
+                },
+                {
+                    name: 'Add a Employee',
+                    value: 'addEmployee' 
+                },
+                {
+                    name: 'Add a role',
+                    value: 'AddRole' 
+                },
+            ]
+            }
+        ]).then((input) => {
+            console.log('Input is: ', input)
+        });
+};
+
+mainSelection();
