2ae79085fcbb100b721bfe0448aa5dfe71531080
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-29 21:23:17 -0300

    Minor changes to server.js will be changing the way that data pulls to string instead of INT

--- server.js
+++ server.js
@@ -17,6 +17,7 @@
 // Should there be a function to warn that you are moving the admin title to someone else? Would be cool.
 
 // Steps: Initialize> Initial menu prompt (Move to new file to keep things simple? Make new module)>
+// ------------------------------------------------------------------------------------------------------------------
 // Set up api's and middleware to handle requests.
 const inquirer = require('inquirer');
 const express = require('express');
@@ -30,26 +31,39 @@ app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  });
+
+// ------------------------------------------------------------------------------------------------------------------
 
 async function main(){
     console.log(chalk.bgRed('Welcome to the Employee Directory \n' ));
     viewOrAddValue = await mainMenuInput()
+    console.log('VieworAddValue is: ', viewOrAddValue)
     viewOrAdd(viewOrAddValue)
     
 };
 //The main function creates a title and then goes straight into mainSelection. Once a selection has been made. It will go to the router middleware
 // Which will determine where the user will be sent.
 
+// Below function checks value of vieworAddValue which equals the users input. Making it match a name rather than a number
+// would be better for future development, but since I know this will not be expanded upon, this will do fine for now.
+// I'm using data as a general pass-term. Is this a bad idea?
 async function viewOrAdd(data){
     switch (data) {
         case 1:
         case 2:
         case 3:
-          console.log('Hit View Something');
           view(data);
           break;
+// ----> View Above, Add Below <----
         case 4:
           console.log('Hit add Department');
+          add(data);
           break;
         case 5:
           console.log('Hit add Employee');
@@ -63,20 +77,22 @@ async function viewOrAdd(data){
       }
       
   };
-
-  const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-  });
-
+// Data will represent a INT through this entire structure. change to string for better tracking and easier query
 function view(data){
-    db.query('SELECT * FROM ', data, function (err, results) {
+  
+    db.query(`SELECT * FROM ${tableName}`, data, function (err, results) {
+      if (err){
+        console.log(err)
+      }else
+        console.log('Hit view function')
         console.log(results);
       });
 }
+// Make tableName into a seperate variable based on data. Do if statement here? Seems kinda ridged.
 
+function add(data){
+    console.log('Hit add function')
+}
 
   sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => main());
